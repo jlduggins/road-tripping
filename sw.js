425036b736928ee452621d5503c9842bad754dae
jlduggins <37988711+jlduggins@users.noreply.github.com>
2026-04-12 20:52:18 -0700

    Add date visited
    Remove arrive and depart dates on log location

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
  * - Network First: API calls, dynamic data
  */
 
-const CACHE_VERSION = 'v42';
+const CACHE_VERSION = 'v50';
 const CACHE_NAME = `the-fallback-${CACHE_VERSION}`;
 
 // Files to pre-cache on install
