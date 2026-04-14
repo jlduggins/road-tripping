84df51cf617ee7d0f882e925c6aa41fc9c022b5b
jlduggins <37988711+jlduggins@users.noreply.github.com>
2026-04-13 20:54:12 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
  * - Network First: API calls, dynamic data
  */
 
-const CACHE_VERSION = 'v60';
+const CACHE_VERSION = 'v62';
 const CACHE_NAME = `the-fallback-${CACHE_VERSION}`;
 
 // Files to pre-cache on install
