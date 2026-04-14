723bd16204a9de18c684fb7e7692fbecaa05338c
jlduggins <37988711+jlduggins@users.noreply.github.com>
2026-04-13 19:54:01 -0700

    Fixes to dashboard
    move dashboard style changes to styles css

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
  * - Network First: API calls, dynamic data
  */
 
-const CACHE_VERSION = 'v57';
+const CACHE_VERSION = 'v58';
 const CACHE_NAME = `the-fallback-${CACHE_VERSION}`;
 
 // Files to pre-cache on install
